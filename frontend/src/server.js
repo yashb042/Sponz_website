43a943cf9460756e0bab8f57c10c6af18e4d0a6a
yash.bansal <yash.bansal@redbus.com>
2024-08-22 11:38:09 +0530

    Server mistake

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-// export const server = "https://sponz-website.vercel.app/api/v2";
-export const server = "http://localhost:8001/api/v2";
+export const server = "https://sponz-website.vercel.app/api/v2";
+// export const server = "http://localhost:8001/api/v2";
 
